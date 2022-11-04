b3724bb75040f3c7403b5ee6f2f6608204ded5d4
jakubbedra <jakubbedra@wp.pl>
2022-11-04 16:37:07 +0100

    chuj

--- TspShared/ThreeOptSolver.cs
+++ TspShared/ThreeOptSolver.cs
@@ -2,11 +2,11 @@
 
 public class ThreeOptSolver
 {
-    private double[][] _cities;
+    private double[,] _cities;
     private int[] _currentBest;
     private int _n;
 
-    public ThreeOptSolver(double[][] cities, int[] initial, int n)
+    public ThreeOptSolver(double[,] cities, int[] initial, int n)
     {
         _cities = cities;
         _currentBest = initial;
@@ -15,20 +15,86 @@ public class ThreeOptSolver
 
     public int[] Solve()
     {
+        dupa();
         return _currentBest;
     }
 
     private int dupa()
     {
+        double delta = 0.0;
         for (int i = 0; i < _n; i++)
         {
-            for (int j = 0; j < _n; j++)
+            //int i1 = i;
+            int x1 = _currentBest[i];
+            int x2 = _currentBest[(i + 1) % _n];
+
+            for (int j = i + 2 /* +i? */; j < _n - 2; j++)
             {
-                for (int k = 0; k < _n; k++)
+                //int j1 = (i1 + j) % _n;
+                int y1 = _currentBest[j];
+                int y2 = _currentBest[(j + 1) % _n];
+
+                for (int k = j + 2; k < _n; k++)
                 {
-                    
+                    //int k1 = (i1 + k) % _n;
+                    int z1 = _currentBest[k];
+                    int z2 = _currentBest[(k + 1) % _n];
+
+                    delta += ReverseIfBetter(i, j, k);
+                    Console.WriteLine(delta);
                 }
             }
         }
+
+        return 0;
+    }
+
+    private double ReverseIfBetter(int i, int j, int k)
+    {
+        int x1 = _currentBest[i];
+        int x2 = _currentBest[(i + 1) % _n];
+
+        int y1 = _currentBest[j];
+        int y2 = _currentBest[(j + 1) % _n];
+
+        int z1 = _currentBest[k];
+        int z2 = _currentBest[(k + 1) % _n];
+
+        var d = TspUtils.Distance;
+
+        double d0 = d(_cities, x1, x2) + d(_cities, y1, y2) + d(_cities, z1, z2);
+        double d1 = d(_cities, x1, y1) + d(_cities, x2, y2) + d(_cities, z1, z2);
+        double d2 = d(_cities, x1, x2) + d(_cities, y1, z1) + d(_cities, y2, z2);
+        double d3 = d(_cities, x1, y2) + d(_cities, z1, x2) + d(_cities, y1, z2);
+        double d4 = d(_cities, z2, x2) + d(_cities, y1, y2) + d(_cities, z1, x1);
+
+        if (d0 > d1)
+        {
+            Array.Reverse(_currentBest, i, j - i);
+            return d1 - d0;
+        }
+        else if (d0 > d2)
+        {
+            Array.Reverse(_currentBest, j, k - j);
+            return d2 - d0;
+        }
+        else if (d0 > d4)
+        {
+            Array.Reverse(_currentBest, i, k - i);
+            return d4 - d0;
+        }
+        else if (d0 > d3)
+        {
+            List<int> tmp = new List<int>();
+            for (int l = j; l < k; l++)
+                tmp.Add(_currentBest[l]);
+            for(int l=i; l<j; l++)
+                tmp.Add(_currentBest[l]);
+            for (var l = 0; l < tmp.Count; l++)
+                _currentBest[l + i] = tmp[l];
+            return d3 - d0;
+        }
+
+        return 0.0;
     }
 }
--- TspShared/TspUtils.cs
+++ TspShared/TspUtils.cs
@@ -8,5 +8,20 @@ public class TspUtils
     {
         return indices.OrderBy(x => _random.Next()).ToArray();
     }
-    
+
+    public static double Distance(double[,] cities, int a, int b)
+    {
+        double x = cities[a, 0] - cities[b, 0];
+        double y = cities[a, 1] - cities[b, 1];
+        return Math.Sqrt(x * x + y * y);
+    }
+
+    public static double TotalTourDistance(double[,] cities, int[] tour)
+    {
+        double ret = 0.0;
+        for (int i = 0; i < tour.Length - 1; i++)
+            ret += Distance(cities, tour[i], tour[i + 1]);
+        ret += Distance(cities, tour[0], tour[tour.Length - 1]);
+        return ret;
+    }
 }
